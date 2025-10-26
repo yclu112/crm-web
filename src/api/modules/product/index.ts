febedb6b8934aa2e7ec6858992fea57de1024633
yclu <1527095504@qq.com>
2025-10-26 17:41:25 +0800

    feat:商品管理

---
+++ src/api/modules/product/index.ts
@@ -0,0 +1,11 @@
+import http from '@/api'
+import { COMMON_ADMIN_API } from '@/api/axios/servicePort'
+
+/**
+ * @name 商品管理
+ */
+export const ProductApi = {
+  page: (params: any) => http.post(COMMON_ADMIN_API + '/product/page', params),
+  saveOrEdit: (params: any) => http.post(COMMON_ADMIN_API + '/product/saveOrEdit', params),
+  remove: (params: any) => http.post(COMMON_ADMIN_API + '/product/remove', params)
+}
